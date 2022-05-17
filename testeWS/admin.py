68c8266dec2e95081ee35006430ad398981d12cd
Rafael L anders <rafaellanders23@gmail.com>
2022-05-16 21:58:51 -0300

    Criando Admin

--- testeWS/admin.py
+++ testeWS/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Modelo, Marca, Carro
+
+admin.site.register(Modelo )
+admin.site.register(Carro)
+admin.site.register(Marca)
 
 # Register your models here.
